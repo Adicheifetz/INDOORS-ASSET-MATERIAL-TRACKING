d7d09f82f2e415e7dfb0527eebe32cddda6445b1
Adicheifetz <141830949+Adicheifetz@users.noreply.github.com>
2023-12-30 18:40:33 +0200

    Add files via upload

--- lab1.js
+++ lab1.js
@@ -1,45 +1,68 @@
-  // Get the canvas element and 2D context
-  var canvas = document.getElementById('uwb-canvas');
-  var ctx = canvas.getContext('2d');
+var canvas = document.getElementById('uwb-canvas');
+var ctx = canvas.getContext('2d');
 
-  // Function to update the tag position on the canvas
-  function updateTagPosition(x, y) {
-      // Clear the previous content of the canvas
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+function updateTagPosition(x, y) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBluePoint(38, 40, 1);
+    drawBluePoint(243, 40, 2);
+    drawBluePoint(53, 141, 3);
+    drawRedPoint(x, y);
 
-      // Draw anchor points
-      ctx.fillStyle = 'blue';
-      ctx.beginPath();
-      ctx.arc(38, 40, 5, 0, 2 * Math.PI);
-      ctx.fill();
+    updateDistanceTable(x, y);
+}
 
-      ctx.beginPath();
-      ctx.arc(243, 40, 5, 0, 2 * Math.PI);
-      ctx.fill();
+function drawBluePoint(x, y, number) {
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.fill();
 
-      ctx.beginPath();
-      ctx.arc(53, 141, 5, 0, 2 * Math.PI);
-      ctx.fill();
+    // Draw the point number inside the blue circle
+    ctx.fillStyle = 'white'; // Set text color
+    ctx.font = '10px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(number, x, y);
+}
 
-      // Draw the updated tag position
-      ctx.fillStyle = 'red';
-      ctx.beginPath();
-      ctx.arc(x, y, 5, 0, 2 * Math.PI);
-      ctx.fill();
-  }
+function drawRedPoint(x, y) {
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.fill();
+}
 
-  // Simulate an update of the tag position (replace this with real data)
-  function simulateUpdate() {
-      // Generate random coordinates for simulation
-      var newX = Math.random() * canvas.width;
-      var newY = Math.random() * canvas.height;
+function calculateDistance(x1, y1, x2, y2) {
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
 
-      // Update the tag position on the canvas
-      updateTagPosition(newX, newY);
+function updateDistanceTable(x, y) {
+    var tableBody = document.getElementById('distance-table-body');
+    tableBody.innerHTML = ''; // Clear existing table rows
 
-      // Set a timer for the next update (every 1500 milliseconds / 1 second)
-      setTimeout(simulateUpdate, 2000);
-  }
+    // Calculate and append distances to the table
+    appendDistanceToTable('Blue Point 1', calculateDistance(x, y, 38, 40));
+    appendDistanceToTable('Blue Point 2', calculateDistance(x, y, 243, 40));
+    appendDistanceToTable('Blue Point 3', calculateDistance(x, y, 53, 141));
+}
 
-  // Start the simulation
-  simulateUpdate();
+function appendDistanceToTable(anchorPoint, distance) {
+    var tableBody = document.getElementById('distance-table-body');
+    var row = tableBody.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    cell1.innerHTML = anchorPoint;
+    cell2.innerHTML = distance.toFixed(2); // Limit decimals to 2 places
+}
+
+function simulateUpdate() {
+    var newX = Math.random() * canvas.width;
+    var newY = Math.random() * canvas.height;
+
+    updateTagPosition(newX, newY);
+
+    //setTimeout(simulateUpdate, 2000);
+}
+
+simulateUpdate();
